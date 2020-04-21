f241866d5ee159670781a1abce0acabfbb39554f
Qian <ee.xuqian@gmail.com>
2020-04-21 16:10:20 -0700

    Initial Commit

---
+++ SuperCard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SuperCard
+//
+//  Created by Qian on 4/21/20.
+//  Copyright © 2020 Stella Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ SuperCard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SuperCard
+//
+//  Created by Qian on 4/21/20.
+//  Copyright © 2020 Stella Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
